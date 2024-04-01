fac4f0d470bcf15fb9776908483b7eba7c757466
MeirJC <meir84@gmail.com>
2024-04-01 23:43:28 +0300

    static markdown to html  functionality completed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,7 @@ class HTMLNode:
         return props_html
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
@@ -25,12 +25,12 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("Invalid HTML: no value")
-        if not self.tag:
+        if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-    
+
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
